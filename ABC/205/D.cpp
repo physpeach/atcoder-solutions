6ed40f29b81366e86fd69612827485ce7d34dbac
physpeach <soup.salad0@gmail.com>
2021-06-20 00:27:07 +0900

    ABC206:{D: AC}

--- ABC/205/D.cpp
+++ ABC/205/D.cpp
@@ -22,24 +22,22 @@ typedef long long ll;
 int main(){
   int N, Q;
   cin >> N >> Q;
-  vector<ll> A(N);
+  vector<ll> A(N), low(N);
 
   REP(i, N){
     cin >> A.at(i);
+    low.at(i) = A.at(i) - 1 - i;
   }
   ll K;
   REP(i, Q){
     cin >> K;
-    int j = 0;
-    while(j < SIZE(A)){
-      if(K + j < A.at(j)){
-        break;
-      }
-      else{
-        j++;
-      }
+    const int j = lower_bound(ALL(low), K) - low.begin();
+    if(j == N){
+      cout << A.at(N - 1) + (K - low.at(N - 1)) << endl;
     }
-    cout << K + j << endl;
+    else{
+      cout << A.at(j) - (low.at(j) - K + 1) << endl;
+    }
   }
   
   return 0;
