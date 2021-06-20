7fd04e7d23abae66faa2638e79b7bc7e14fcc3fa
physpeach <soup.salad0@gmail.com>
2021-06-20 15:23:36 +0900

    common_90: {d3_014: AC}

---
+++ Common_90/d3_014_we_used_to_sing_a_song_together.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+#define FOR(i, L, R) for(ll i = ll(L); i < ll(R); i++)
+#define FORD(i, L, R) for(ll i = ll(L-1); i >= ll(R); i--)
+#define REP(i, N) for(ll i = 0; i < ll(N); i++)
+#define REPD(i, N) for(ll i = ll(N-1); i >= 0; i--)
+#define ALL(arr) arr.begin(), arr.end()
+#define SIZE(arr) ll(arr.size())
+#define INF32 2147483647
+#define INF64 9223372036854775807
+
+//char '0-9': 48-57, 'A-M,N-Z': 65-77,78-90,
+//     'a-m,n-z': 97-109,110-122
+
+//setprecision(10)
+
+int main(){
+
+  int N;
+  cin >> N;
+  vector<ll> A(N), B(N);
+  REP(i, N){
+    cin >> A.at(i);
+  }
+  REP(i, N){
+    cin >> B.at(i);
+  }
+  sort(ALL(A));
+  sort(ALL(B));
+  ll unconfort = 0;
+  REP(i, N)
+    unconfort += abs(A.at(i) - B.at(i)); 
+
+  cout << unconfort << endl;
+
+  return 0;
+}
