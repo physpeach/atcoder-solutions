180f69df897a96d287485ac44701a7298039082a
physpeach <soup.salad0@gmail.com>
2021-07-10 21:46:22 +0900

    ABC209: {A, B: AC}

---
+++ ABC/209/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+#define mcfor(i, L, R) for(ll i = ll(L); i < ll(R); i++)
+#define mcford(i, L, R) for(ll i = ll(L-1); i >= ll(R); i--)
+#define mcrep(i, N) for(ll i = 0; i < ll(N); i++)
+#define mcrepd(i, N) for(ll i = ll(N-1); i >= 0; i--)
+#define mcall(arr) arr.begin(), arr.end()
+#define mcsize(arr) ll(arr.size())
+#define INF32 2147483647
+#define INF64 9223372036854775807
+
+//char '0-9': 48-57, 'A-M,N-Z': 65-77,78-90,
+//     'a-m,n-z': 97-109,110-122
+
+//setprecision(10)
+
+ 
+int main(){
+  int N, X;
+  cin >> N >> X;
+
+  int a;
+
+  X += N / 2;
+  mcrep(i, N){
+    cin >> a;
+    X -= a;
+  }
+  if(X >= 0){
+    cout << "Yes" << endl;
+  }
+  else{
+    cout << "No" << endl;
+  }
+
+  return 0;
+}
